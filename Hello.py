5f252e93f4a9d3ad079c16c3ea56ded0a112dc13
SahaSG552 <SahaSG552@mail.ru>
2021-11-15 22:52:49 +0300

    Init commit

--- Hello.py
+++ Hello.py
@@ -1,5 +1,5 @@
 # number = input()
-number = "216 39 87 0 0 1 0".split()
+number = "216 39 87 0 0 1 1".split()
 for i in range (1, len(number), 2):
     number[i], number[i-1] = number[i-1], number[i]
 print(*number)
