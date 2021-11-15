67197ff152f44769ea8aa0020ef58a0b5379155a
SahaSG552 <SahaSG552@mail.ru>
2021-11-15 22:10:14 +0300

    Init commit

--- Hello.py
+++ Hello.py
@@ -1,9 +1,5 @@
 # number = input()
-number = "8 78 79 6 70 7 58 5".split()
-count = 0
-i = 1
-while i < len(number):
-    if int(number[i]) > int(number[i-1]):
-        count += 1
-    i += 1
-print(count)
+number = "216 39 87 0 0 1 0".split()
+for i in range (1, len(number), 2):
+    number[i], number[i-1] = number[i-1], number[i]
+print(*number)
