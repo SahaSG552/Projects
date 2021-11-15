9268816d121de7b50d94657c42db6410cec05e41
SahaSG552 <SahaSG552@mail.ru>
2021-11-15 21:58:36 +0300

    Init commit

--- Hello.py
+++ Hello.py
@@ -1,5 +1,9 @@
-import string
-print(string.digits)
-print(string.punctuation)
-print(string.ascii_letters)
-print(string.ascii_lowercase)
+# number = input()
+number = "8 78 79 6 70 7 58 5".split()
+count = 0
+i = 1
+while i < len(number):
+    if int(number[i]) > int(number[i-1]):
+        count += 1
+    i += 1
+print(count)
