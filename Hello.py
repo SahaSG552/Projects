b9ca8bfb1d2cca68b2c3f439b3ecbb5c22cdd898
SahaSG552 <sahasg552@mail.ru>
2021-11-17 23:17:00 +0300

    Second commit

--- Hello.py
+++ Hello.py
@@ -1,5 +1,12 @@
-# number = input()
-number = "216 39 87 0 0 1 2".split()
-for i in range (1, len(number), 2):
-    number[i], number[i-1] = number[i-1], number[i]
-print(*number)
+figure = ["ножницы", "бумага", "камень", "ящерица", "Спок"]
+name = ("Тимур", "Руслан")
+# t_figure, r_figure = input(), input()
+t_figure, r_figure = "камень", "бумага"
+# сдвигаем список так, чтобы то, что показывает Тимур было первым в списке
+# из этого списка выбираем 2 элемента от 1 индекса с шагом 2 - Тимур побеждает, если Руслан показывает что то из этих 2х элементов
+win = [(figure[figure.index(t_figure):]+figure[:figure.index(t_figure)])[i] for i in range(1, len(figure), 2)]
+
+if t_figure == r_figure:
+    print("ничья")
+else:
+    print(name[r_figure not in win])
