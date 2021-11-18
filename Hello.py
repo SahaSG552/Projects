7f2ba4439205ae73a859993d4f4dad675be4e26a
SahaSG552 <sahasg552@mail.ru>
2021-11-18 23:29:24 +0300

    Update

--- Hello.py
+++ Hello.py
@@ -3,6 +3,7 @@ word = name + ' запретил букву'
 letters = [_ for _ in set(word) if _.isalpha()]
 letters.sort()
 
+
 for i in letters:
     print(word, i)
     word = word.replace(i, '').strip(' ').replace('  ', ' ')
