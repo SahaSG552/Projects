40ec7d124f44374649c657816624b3c1a340b2c2
SahaSG552 <sahasg552@mail.ru>
2021-11-18 23:33:32 +0300

    Update

--- Hello.py
+++ Hello.py
@@ -2,8 +2,6 @@ name = 'госдеп'
 word = name + ' запретил букву'
 letters = [_ for _ in set(word) if _.isalpha()]
 letters.sort()
-
-
 for i in letters:
     print(word, i)
     word = word.replace(i, '').strip(' ').replace('  ', ' ')
