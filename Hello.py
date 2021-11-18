a4915fa402b6e525351363d0558cb27067daaf8a
SahaSG552 <sahasg552@mail.ru>
2021-11-18 22:02:22 +0300

    Roskomnadzor

--- Hello.py
+++ Hello.py
@@ -1,21 +1,6 @@
-n = int(input())
-refrigerators = []
-b = [c for c in 'anton']
-for virus in range(n):
-    #отсеиваем лишние символы
-    a = [i for i in input() if i.isalpha() and i in b]
-    i = 0
-    k = 0
-    count = 0
-    while i < len(a):
-        if count == 5: 
-            count = 0
-            refrigerators.append(virus)
-            break
-        if a[i] == b[k]:
-            k +=1
-            count += 1
-            continue
-        else:
-            i += 1
-print(*refrigerators)
+name = 'роскомнадзор'
+letters = [_ for _ in set(name)]
+letters.sort()
+for i in letters:
+    print('')
+print(letters)
