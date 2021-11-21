e08159ae557a681ceb7620b5363c3af5b7179e05
SahaSG552 <SahaSG552@mail.ru>
2021-11-21 20:03:55 +0300

    Pascal Triangle

--- Hello.py
+++ Hello.py
@@ -1,8 +1,7 @@
-name = 'васёк'
-word = name + ' запретил букву'
-letters = [_ for _ in set(word) if _.isalpha()]
-letters.sort()
-for i in letters:
-    print(word, i)
-    word = word.replace(i, '').strip(' ').replace('  ', ' ')
-print('Hello, GitHub!')
+a = int(input())
+b = [[1]*i for i in range (a+1)]
+
+for i in range(0, len(b)):
+    for j in range(1,len(b[i])-1):
+        b[i][j] = b[i-1][j-1] + b[i-1][j]
+    if i > 0: print(*b[i])
