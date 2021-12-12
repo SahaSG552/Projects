7625a410922f4328ceecd8c6dfe819a8cac92925
SahaSG552 <SahaSG552@mail.ru>
2021-12-12 22:46:44 +0300

    Swap columns in table

--- Hello.py
+++ Hello.py
@@ -1,17 +1,16 @@
 rows, cols = 3, 4
-matrix = ['0 1 -2 4'.split(),
-        '1 2 5 5'.split(),
-        '5 1 8 9'.split()]
+matrix = ['11 12 13 14'.split(),
+        '21 22 23 24'.split(),
+        '31 32 33 34'.split()]
 matrix = [[int(i) for i in matrix[k]] for k in range(rows)]
-
-
+swap_cols = "0 1"
+swap_cols = [int(i) for i in swap_cols.split()]
 
 # rows, cols = int(input()), int(input())
 # matrix = [[int(i) for i in input().split()] for _ in range(rows)]
-maximum = -9999999999
-for i in range(rows):
-	if max(matrix[i]) > maximum:
-		maximum = max(matrix[i])
-		find_row, find_col = i, matrix[i].index(maximum)
+# swap_cols = [int(i) for i in input().split()]
 
-print(find_row, find_col)
+
+for k in range(rows):
+	matrix[k][swap_cols[0]], matrix[k][swap_cols[1]] = matrix[k][swap_cols[1]], matrix[k][swap_cols[0]]
+	print(*matrix[k], sep=' ')
