8caee87005845bde8a119f39e4863bff3e59d878
SahaSG552 <SahaSG552@mail.ru>
2021-12-12 16:49:28 +0300

    Bigger Then Average commit

--- Excel.py
+++ Excel.py
@@ -5,3 +5,6 @@ ws = wb['Data(Armadi)']
 
 for row in ws.iter_rows(min_row=1, max_col=13, max_row=1, values_only=True):
     print(row)
+
+import sys
+print(*[i.replace('\\', "\"") for i in sys.path])
