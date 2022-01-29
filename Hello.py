d15c53e3f468b548d13abc07d87c2a3fddc129dd
SahaSG552 <SahaSG552@mail.ru>
2022-01-29 16:51:46 +0300

    Add prefix using Dict

--- Hello.py
+++ Hello.py
@@ -1,19 +1,19 @@
-text = """London is the capital of Great Britain.
-        More than six million people live in London.
-        London lies on both banks of the river Thames.
-        It is the largest city in Europe and one of the largest cities in the world. 
-        London is not only the capital of the country, it is also a very big port, 
-        one of the greatest commercial centres in the world, a university city, 
-        and the seat of the government of Great Britain!""".lower().split()
-text = [t.strip(',.!?:;') for t in text]
-
-# result = dict.fromkeys(text,0)
-# for word in text:
-#     if word in result:
-#         result[word] += 1
+identificators = "a a a a a a a aa a a a a a aa a a! b b2 b4 b3 a4 a9 a1 a a!".split()
 
 result = {}
-for word in text:
-    result[word] = result.get(word, 0) + 1
+for word in identificators:
+    result[word] = result.get(word, -1) + 1
+    if result[word] == 0:
+        print(word, end=" ")
+    else:
+        print(word + "_" + str(result[word]), end=" ")
+print()
 
-print(sorted([i for i in result.keys() if result[i] == min(result.values())])[0])
+for i in identificators:
+    count = 0
+    for j in range(len(identificators)):
+        if identificators[j] == i:
+            identificators[j] += "_" + str(count)
+            count +=1
+            identificators[j] = identificators[j].replace("_0","")
+print(*identificators)
