f6fb4e7f83d9c68691e6786c184c99ba5d31d61f
SahaSG552 <SahaSG552@mail.ru>
2022-01-30 20:00:11 +0300

    Dicts of Dicts with generator

--- Hello.py
+++ Hello.py
@@ -1,12 +1,13 @@
-a = ['Вижу зверей', 'Живу резвей']
-#a = [sorted([i.lower() for i in a if i.isalnum()]) for _ in range(2)]
-#print("YES" if a[0] == a[1] else "NO")
+import pprint
+student_ids = ['S001', 'S002', 'S003', 'S004',
+               'S005', 'S006','S007', 'S008',
+               'S009', 'S010', 'S011', 'S012','S013'] 
+student_names = ['Camila Rodriguez', 'Juan Cruz', 'Dan Richards', 'Sam Boyle',
+                 'Batista Cesare', 'Francesco Totti', 'Khalid Hussain', 'Ethan Hawke',
+                 'David Bowman', 'James Milner', 'Michael Owen',
+                 'Gary Oldman', 'Tom Hardy'] 
+student_grades = [86, 98, 89, 92, 45, 67, 89, 90, 100, 98, 10, 96, 93]
 
-
-def dicts(sentence):
-    my_dict = {}
-    for i in sentence.lower():
-        if i.isalpha():
-            my_dict[i] = my_dict.get(i, 0) + 1
-    return(my_dict)
-print(("NO", "YES")[dicts('Вижу зверей') == dicts('Живу резвей')])
+# result = [{student_ids[i]: {student_names[i]: student_grades[i]}} for i in range(len(student_ids))]
+result = [{key:{key1:value1}} for key, key1, value1 in zip(student_ids, student_names, student_grades)]
+pprint.pprint(result)
