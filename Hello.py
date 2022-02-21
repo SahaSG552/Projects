6a51236980fa294b6f9579d0168b1572dec21771
SahaSG552 <sahasg552@mail.ru>
2022-02-22 00:06:02 +0300

    function with **kwargs

--- Hello.py
+++ Hello.py
@@ -1,13 +1,6 @@
-import pprint
-student_ids = ['S001', 'S002', 'S003', 'S004',
-               'S005', 'S006','S007', 'S008',
-               'S009', 'S010', 'S011', 'S012','S013'] 
-student_names = ['Camila Rodriguez', 'Juan Cruz', 'Dan Richards', 'Sam Boyle',
-                 'Batista Cesare', 'Francesco Totti', 'Khalid Hussain', 'Ethan Hawke',
-                 'David Bowman', 'James Milner', 'Michael Owen',
-                 'Gary Oldman', 'Tom Hardy'] 
-student_grades = [86, 98, 89, 92, 45, 67, 89, 90, 100, 98, 10, 96, 93]
+def info_kwargs(**kwargs):
+    for k, v in sorted(kwargs.items()):
+        print(f"{k}: {v}")
 
-# result = [{student_ids[i]: {student_names[i]: student_grades[i]}} for i in range(len(student_ids))]
-result = [{key:{key1:value1}} for key, key1, value1 in zip(student_ids, student_names, student_grades)]
-pprint.pprint(result)
+
+info_kwargs(first_name="Timur", last_name="Guev", age=28, job="teacher")
--- replit_com.py
+++ replit_com.py
@@ -1,9 +1,9 @@
-a = int(input())
+a = 5
 summa = a
-#digits = len(str(a))
-for i in range (len(str(a))):
-  summa1 = summa
-  for digit in str(summa1):
-    summa += int(digit)
-  print(summa)
+# digits = len(str(a))
+for i in range(len(str(a))):
+    summa1 = summa
+    for digit in str(summa1):
+        summa += int(digit)
+    print(summa)
 print(summa1)
