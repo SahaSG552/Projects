09dded48eb4f5d262f456bdd8b0b9486433366a8
SahaSG552 <sahasg552@mail.ru>
2022-12-14 01:21:19 +0300

    probability of a group of people having the same birthday

--- Hello.py
+++ Hello.py
@@ -1,4 +1,25 @@
-a = [0, 1, 2, 3]
-print(a[-1])
-for i in a:
-    print(i, end=' ')
+from random import choices
+
+
+def get_int(message):
+    while True:
+        try:
+            return int(input(message))
+        except ValueError:
+            print("Введите целочисленное значение")
+
+
+# birthdays_range = 20
+people = [_ for _ in range(1, 366)]
+birthdays_range = get_int("Введите количество людей: ")
+iterations = get_int("Количество итераций: ")
+counter = 0  # считаем совпадения
+for i in range(iterations):
+    birthdays = choices(people, k=birthdays_range)
+    counter += len(set(birthdays)) == birthdays_range
+
+print(
+    f"Вероятность, что из {birthdays_range} людей "
+    f"у кого-то совпадёт день рождения: "
+    f"{round(((iterations-counter)*100)/iterations, 5)}%"
+)
