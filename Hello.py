5f1c4c8f5e3e278a9ec33ae0e1587f47bafa3b56
SahaSG552 <sahasg552@mail.ru>
2022-12-17 10:26:49 +0300

    sys module

--- Hello.py
+++ Hello.py
@@ -1,25 +1,7 @@
-from random import choices
+import sys
 
+if len(sys.argv) < 2:
+    sys.exit("Too few arguments")
 
-def get_int(message):
-    while True:
-        try:
-            return int(input(message))
-        except ValueError:
-            print("Введите целочисленное значение")
-
-
-# birthdays_range = 20
-people = [_ for _ in range(1, 366)]
-birthdays_range = get_int("Введите количество людей: ")
-iterations = get_int("Количество итераций: ")
-counter = 0  # считаем совпадения
-for i in range(iterations):
-    birthdays = choices(people, k=birthdays_range)
-    counter += len(set(birthdays)) == birthdays_range
-
-print(
-    f"Вероятность, что из {birthdays_range} людей "
-    f"у кого-то совпадёт день рождения: "
-    f"{round(((iterations-counter)*100)/iterations, 5)}%"
-)
+for arg in sys.argv[1:]:
+    print("hello, my name is", arg)
