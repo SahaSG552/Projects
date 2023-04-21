7bcb570d33d6d932bad6b5dfa2be7e61b71a1b78
SahaSG552 <sahasg552@mail.ru>
2023-04-21 19:58:44 +0300

    111

--- CS50 Introduction/OOP/Bank.py
+++ CS50 Introduction/OOP/Bank.py
@@ -1,1 +1,24 @@
-help()
+import random
+import string
+
+
+num = int(input())
+
+
+def generate_string(num):
+    return ("").join(random.choice(string.ascii_letters) for _ in range(num))
+
+
+random_string = generate_string(num)
+
+
+def output_vowel_freq(random_string):
+    vowel_count = sum(letter.lower() in {"a", "e", "i", "o", "u"}
+                      for letter in random_string)
+    return vowel_count / len(random_string)
+
+
+proportion_of_vowels = output_vowel_freq(random_string)
+
+print(random_string)
+print(proportion_of_vowels)
