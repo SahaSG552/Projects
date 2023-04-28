8515c95bc698f72e2923df31e425ebeefc298f53
SahaSG552 <sahasg552@mail.ru>
2023-04-28 18:57:51 +0300

    dataclasses and namedtuples

--- Hello.py
+++ Hello.py
@@ -1,7 +1,24 @@
-import sys
+from dataclasses import dataclass
+from collections import namedtuple
 
-if len(sys.argv) < 2:
-    sys.exit("Too few arguments")
 
-for arg in sys.argv[1:]:
-    print("hello, my name is", arg)
+@dataclass
+class Position:
+    name: str
+    x: float
+    y: float
+    
+
+city = Position("Norge", 25.3, 46.5)
+
+print(city)
+print(city.name)
+print(city.x)
+print(city.y)
+
+NamedTuplePosition = namedtuple("NamedTuplePosition", ["name", "x", "y"])
+city1 = NamedTuplePosition("Norge", 25.3, 46.5)
+print(city1)
+print(city1.name)
+print(city1.x)
+print(city1.y)
