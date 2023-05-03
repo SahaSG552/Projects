f4d50fa321f060d59d61b78c486cf6e2e8395161
SahaSG552 <sahasg552@mail.ru>
2023-05-03 23:07:22 +0300

    Iterator

--- Hello.py
+++ Hello.py
@@ -1,24 +1,19 @@
-from dataclasses import dataclass
-from collections import namedtuple
+class It:
+    def __init__(self):
+        self.l = list(range(1, 11))
+        self.current_index = 0
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.current_index > len(self.l) - 1:
+            raise StopIteration
+        value = self.l[self.current_index]
+        self.current_index += 1
+        return value
 
-
-@dataclass
-class Position:
-    name: str
-    x: float
-    y: float
-
-
-city = Position("Norge", 25.3, 46.5)
-
-print(city)
-print(city.name)
-print(city.x)
-print(city.y)
-
-NamedTuplePosition = namedtuple("NamedTuplePosition", ["name", "x", "y"])
-city1 = NamedTuplePosition("Norge", 25.3, 46.5)
-print(city1)
-print(city1.name)
-print(city1.x)
-print(city1.y)
+g = It()
+print(g.__next__())
+for x in g:
+    print(x, end=' ')
