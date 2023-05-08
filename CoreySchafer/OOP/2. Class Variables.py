7874cf0d0118ed71723c1211607e6d7ebe9918d2
SahaSG552 <sahasg552@mail.ru>
2023-05-08 15:18:22 +0300

    classmethod & staticmethod

--- CoreySchafer/OOP/2. Class Variables.py
+++ CoreySchafer/OOP/2. Class Variables.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 class Employee:
     # Class Attributes
     num_of_employees = 0  # number of employees
@@ -8,7 +11,7 @@ class Employee:
         self.last = last
         self.pay = pay
         self.email = f"{first}{last}@company.com"
-        Employee.num_of_employees += 1
+        type(self).num_of_employees += 1
 
     def fullname(self):
         return f"{self.first} {self.last}"
@@ -16,15 +19,33 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
 
+    @classmethod
+    def set_raise_amount(cls, amount):
+        cls.raise_amount = amount
 
-emp_1 = Employee("Aleks", "Zander", 50000)
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split("-")
+        return cls(first, last, pay)   
+
+    @staticmethod
+    def is_workday(day) -> bool:
+        return day.weekday() < 5
+
+
+emp_1 = Employee("Creed", "Bratton", 50000)
 emp_2 = Employee("Andrew", "Bernard", 50000)
-emp_1.raise_amount = 1.05
+emp_3 = Employee.from_string("Stanley-Huddson-1000")
+print(emp_3.__dict__)
+# emp_1.raise_amount = 1.05
 emp_1.raise_amount = 1.06
+Employee.set_raise_amount(1.07)
 emp_1.apply_raise()
-# emp_2.apply_raise()
+emp_2.apply_raise()
 
 print(Employee.num_of_employees)
-
 print(emp_1.fullname(), emp_1.raise_amount, emp_1.pay)
 print(emp_2.fullname(), emp_2.raise_amount, emp_2.pay)
+
+my_date = datetime.date(2023, 5, 12)
+print(Employee.is_workday(my_date))
