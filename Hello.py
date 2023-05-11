5fe76e5f617a0623153c00453925826e056dd1e3
SahaSG552 <sahasg552@mail.ru>
2023-05-11 17:04:54 +0300

    Щупаю Abstract method

--- Hello.py
+++ Hello.py
@@ -1,19 +1,7 @@
-class It:
-    def __init__(self):
-        self.l = list(range(1, 11))
-        self.current_index = 0
-    
-    def __iter__(self):
-        return self
-    
-    def __next__(self):
-        if self.current_index > len(self.l) - 1:
-            raise StopIteration
-        value = self.l[self.current_index]
-        self.current_index += 1
-        return value
+from abc import ABC, abstractmethod
 
-g = It()
-print(g.__next__())
-for x in g:
-    print(x, end=' ')
+
+class CallDevice(ABC):
+    @abstractmethod
+    def call():
+        pass
