c003daa6ab0af59839a08273d761160b5fe83e46
SahaSG552 <sahasg552@mail.ru>
2023-06-27 12:04:26 +0300

    second solution (after googling)

--- Leetcode/13.roman-to-integer.py
+++ Leetcode/13.roman-to-integer.py
@@ -15,44 +15,12 @@ class Solution:
                          "D": 500,
                          "M": 1000
                          }
-        calc = 0
-        i = 0
-        while i < len(s):
-            if s[i] == "I" and len(s) - i > 1:
-                if s[i+1] == "V":
-                    calc += 4
-                    i += 2
-                elif s[i+1] == "X":
-                    calc += 9
-                    i += 2
-                else:
-                    calc += roman_numbers[s[i]]
-                    i += 1
 
-            elif s[i] == "X" and len(s) - i > 1:
-                if s[i+1] == "L":
-                    calc += 40
-                    i += 2
-                elif s[i+1] == "C":
-                    calc += 90
-                    i += 2
-                else:
-                    calc += roman_numbers[s[i]]
-                    i += 1
-            elif s[i] == "C" and len(s) - i > 1:
-                if s[i+1] == "D":
-                    calc += 400
-                    i += 2
-                elif s[i+1] == "M":
-                    calc += 900
-                    i += 2
-                else:
-                    calc += roman_numbers[s[i]]
-                    i += 1
-            else:
-                calc += roman_numbers[s[i]]
-                i += 1
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
 
-        return calc
+        return sum(roman_numbers[c] for c in s)
+
 
 # @lc code=end
